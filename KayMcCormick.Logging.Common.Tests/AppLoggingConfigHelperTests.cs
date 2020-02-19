1ec15252a07a6313aaded4e5bef8e40a2cd104bb
GitTask <gittask@notused.com>
2020-02-18 21:24:48 -0800

    netcore conversion

---
+++ KayMcCormick.Logging.Common.Tests/AppLoggingConfigHelperTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using KayMcCormick.Logging.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using NLog.Layouts;
+
+namespace KayMcCormick.Logging.Common.Tests
+{
+    public class AppLoggingConfigHelperTests
+    {
+       
+    }
+}
---
+++ KayMcCormick.Logging.Common.Tests/DefaultCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KayMcCormick.Test.Common.Fixtures ;
+using Xunit ;
+
+namespace KayMcCormick.Logging.Common.Tests
+{
+    [CollectionDefinition("Default")]
+    class DefaultCollection : ICollectionFixture <GlobalLoggingFixture>
+    {
+    }
+}
